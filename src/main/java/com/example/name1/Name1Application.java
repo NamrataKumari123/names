200b352353e67cf4a5b87ca20e97635a1732cab8
namrata <namrata19051996@gmail.com>
2022-10-30 16:18:56 +0530

    my project

---
+++ src/main/java/com/example/name1/Name1Application.java
@@ -0,0 +1,13 @@
+package com.example.name1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Name1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Name1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/name1/Name1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.name1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Name1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
